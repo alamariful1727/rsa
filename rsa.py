a25954c1c50eb476429825e21ef08cb46339c9f1
Ariful Alam <alamariful1727@gmail.com>
2022-02-19 22:15:22 -0500

    isprime & generate_p_and_q updated

--- rsa.py
+++ rsa.py
@@ -1,44 +1,30 @@
-from math import sqrt
 import random
 
-def isprime(n):
-    if n < 2:
-        return False
-    elif n == 2:
+def isprime(num):
+    if num == 2:
         return True
-    else:
-        for i in range(2, int(sqrt(n)) + 1, 2):
-            if n % i == 0:
-                return False
+    if num < 2 or num % 2 == 0:
+        return False
+    for n in range(3, int(num**0.5)+2, 2):
+        if num % n == 0:
+            return False
     return True
 
 def generate_p_and_q(keysize):
     nMin = 1 << (keysize - 1)
     nMax = (1 << keysize) - 1
-    primes = [2]
-
-    # print("nMin", nMin)
-    # print("nMax", nMax)
-
-    for i in range(3, nMax, 2):
-        for p in primes:
-            if i % p == 0:
-                break
-        else:
-            primes.append(i)
+    primes = [i for i in range(nMin+1, nMax, 2) if isprime(i)]
 
     # print("primes",primes)
     # print("len(primes)",len(primes))
 
-    filteredPrimes = [prime for prime in primes if prime > nMin]
-    # print("len(filteredPrimes)",len(filteredPrimes))
-
-    p = random.choice(filteredPrimes)
-    filteredPrimes.remove(p)
-    q = random.choice(filteredPrimes)
+    p = random.choice(primes)
+    primes.remove(p)
+    q = random.choice(primes)
 
     return (p, q)
 
 if __name__ == "__main__":
-    # p, q = generate_p_and_q(16)
-    p, q = (52457, 39509)
+    p, q = generate_p_and_q(16)
+    # p, q = (52457, 39509)
+    print(p, q)
