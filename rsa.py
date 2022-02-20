408fc67b065e6238d20f1286de2a3c36749547f7
Ariful Alam <alamariful1727@gmail.com>
2022-02-20 02:09:19 -0500

    Choose an integer 'e' such that 'e' and phi(n) are coprime

--- rsa.py
+++ rsa.py
@@ -24,7 +24,32 @@ def generate_p_and_q(keysize):
 
     return (p, q)
 
+def gcd(x, y):
+    while y != 0:
+        x, y = y, x % y
+    return x
+
+def generate_keys(p, q):
+    # Calculate N = p * q
+    N = p*q
+    print("N ", N)
+
+    # Calculate Phi(N)
+    phi = (p-1) * (q-1) 
+    print("phi ", phi)
+
+    # Choose an integer 'e' such that 'e' and phi(n) are coprime
+    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+    print("e ", e)
+
 if __name__ == "__main__":
     p, q = generate_p_and_q(16)
-    # p, q = (52457, 39509)
+    p, q = (52457, 39509)
     print(p, q)
+
+    generate_keys(p, q)
