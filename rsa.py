f009be7e34f6f4c468003d15f9fa405c981be3ed
Ariful Alam <alamariful1727@gmail.com>
2022-02-20 20:30:08 -0500

    keys genaration

--- rsa.py
+++ rsa.py
@@ -1,5 +1,6 @@
 import random
 
+''' check prime number '''
 def isprime(num):
     if num == 2:
         return True
@@ -10,6 +11,7 @@ def isprime(num):
             return False
     return True
 
+''' generate p and q as per the given keysize '''
 def generate_p_and_q(keysize):
     nMin = 1 << (keysize - 1)
     nMax = (1 << keysize) - 1
@@ -24,11 +26,24 @@ def generate_p_and_q(keysize):
 
     return (p, q)
 
+''' calculate gcd '''
 def gcd(x, y):
     while y != 0:
         x, y = y, x % y
     return x
 
+''' calculate the modular inverse from e and phi '''
+def mod_inverse(e, phi):
+    # d -> ed = 1(mod z)        ; 1 < d < z
+    d = 2
+    while d < phi:
+        # check if this is the required `d` value
+        if ((d*e) % phi)==1:
+            return d
+        # else : increment and continue
+        d += 1
+
+''' generate public and private keys '''
 def generate_keys(p, q):
     # Calculate N = p * q
     N = p*q
@@ -40,6 +55,7 @@ def generate_keys(p, q):
 
     # Choose an integer 'e' such that 'e' and phi(n) are coprime
     # Use Euclid's Algorithm to verify that e and phi(n) are coprime
+    # e -> gcd(e,z)==1      ; 1 < e < z
     e = random.randrange(1, phi)
     g = gcd(e, phi)
     while g != 1:
@@ -47,9 +63,20 @@ def generate_keys(p, q):
         g = gcd(e, phi)
     print("e ", e)
 
-if __name__ == "__main__":
-    p, q = generate_p_and_q(16)
-    p, q = (52457, 39509)
-    print(p, q)
+    d = mod_inverse(e, phi)
+    print("d ", d)
 
-    generate_keys(p, q)
+    public_key = (e, N)
+    private_key = (d, N)
+
+    return (public_key, private_key)
+
+if __name__ == "__main__":
+    # p, q = generate_p_and_q(16)
+    p, q = (52457, 39509)
+    print("p: ", p)
+    print("q: ", q)
+
+    public, private = generate_keys(p, q)
+    print("Public Key: ", public)
+    print("Private Key: ", private)
