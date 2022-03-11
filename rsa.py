da301d5c7ed94072e8153f03ecadbbd40b681848
nimmitahsin1727 <nimmitahsin1727@gmail.com>
2022-03-11 18:46:31 -0500

    utility: reset text file

--- rsa.py
+++ rsa.py
@@ -1,5 +1,10 @@
 import random
 
+''' reset file '''
+def reset_file(filename="myData.txt"):
+    with open(filename, "w") as f:
+        f.write("")
+
 ''' write data to file '''
 def write_data(data, filename="myData.txt"):
     with open(filename, "a") as f:
@@ -17,21 +22,6 @@ def isprime(num):
             return False
     return True
 
-''' generate p and q as per the given keysize '''
-def generate_p_and_q(keysize):
-    nMin = 1 << (keysize - 1)
-    nMax = (1 << keysize) - 1
-    primes = [i for i in range(nMin+1, nMax, 2) if isprime(i)]
-
-    # print("primes",primes)
-    # print("len(primes)",len(primes))
-
-    p = random.choice(primes)
-    primes.remove(p)
-    q = random.choice(primes)
-
-    return (p, q)
-
 ''' calculate gcd '''
 def gcd(x, y):
     while y != 0:
@@ -40,7 +30,7 @@ def gcd(x, y):
 
 ''' calculate the modular inverse from e and phi '''
 def mod_inverse(e, phi):
-    # d -> ed = 1(mod z)        ; 1 < d < z
+    # d -> ed = 1(mod phi)        ; 1 < d < phi
     d = 2
     while d < phi:
         # check if this is the required `d` value
@@ -48,6 +38,18 @@ def mod_inverse(e, phi):
             return d
         # else : increment and continue
         d += 1
+
+''' generate p and q as per the given keysize '''
+def generate_p_and_q(keysize):
+    nMin = 1 << (keysize - 1)
+    nMax = (1 << keysize) - 1
+    primes = [i for i in range(nMin+1, nMax, 2) if isprime(i)]
+
+    p = random.choice(primes)
+    primes.remove(p)
+    q = random.choice(primes)
+
+    return (p, q)
 
 ''' generate public and private keys '''
 def generate_keys(p, q):
@@ -61,11 +63,11 @@ def generate_keys(p, q):
 
     # Choose an integer 'e' such that 'e' and phi(n) are coprime
     # Use Euclid's Algorithm to verify that e and phi(n) are coprime
-    # e -> gcd(e,z)==1      ; 1 < e < z
+    # e -> gcd(e, phi) == 1      ; 1 < e < phi
     e = random.randrange(1, phi - 1)
     g = gcd(e, phi)
     while g != 1:
-        e = random.randrange(1, phi)
+        e = random.randrange(1, phi - 1)
         g = gcd(e, phi)
     write_data('e = '+str(e))
 
@@ -78,8 +80,12 @@ def generate_keys(p, q):
     return (public_key, private_key)
 
 if __name__ == "__main__":
+    # utility: reset text file
+    reset_file()
+
     p, q = generate_p_and_q(16)
     # p, q = (52457, 39509)
+    
     write_data('p = '+str(p))
     write_data('q = '+str(q))
 
